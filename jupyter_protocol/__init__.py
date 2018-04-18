53dcf8d9cd352783639e55023ef0e14a745dfa7d
Thomas Kluyver <thomas@kluyver.me.uk>
2018-04-18 20:37:21 +0200

    Start removing kernel management stuff
    I'm splitting this into a separate package, jupyter_kernel_mgmt

--- jupyter_protocol/__init__.py
+++ jupyter_protocol/__init__.py
@@ -2,8 +2,5 @@
 
 from ._version import version_info, __version__, protocol_version_info, protocol_version
 from .connect import *
-from .launcher import *
 from .client import KernelClient
-from .manager import KernelManager, run_kernel
 from .blocking import BlockingKernelClient
-from .multikernelmanager import MultiKernelManager
